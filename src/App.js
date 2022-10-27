570e21fdcd4b2b3d1a5f251ad71f05333d4136d3
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-27 16:15:43 -0400

    more styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <MovieList />
+      <Footer />
     </div>
   );
 }
