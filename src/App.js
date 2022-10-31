5552074bd3684eef9ed38320d668fdadf40e4546
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-31 11:11:49 -0400

    first props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import Footer from './components/Footer';
+import MovieComponent from './components/MovieComponent';
 import './App.css';
 
 function App() {
+
+  const movieInfo = {
+    id: "1",
+    posterURL: "https://bocacode.com/assets/images/bc-logos/boca-code-rgb-logo-tag-reversed.png",
+    title: "Boca Code; The Movie"  
+  }
+
   return (
     <div className="App">
       <Header />
+      <MovieComponent movieProp={movieInfo} />
       <MovieList />
       {/* we will change this */}
       <Footer />
